f91e1307b1ea779e3015d6636acda04a29817920
Paul Welsh <spacedawwwg@gmail.com>
2019-09-22 09:51:33 +0100

    update form to use google sheets

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="register-form">
+  <div class="register-form" :class="{ 'register-form--submitting' : submitting }">
     <ValidationObserver tag="div" ref="observer" v-slot="{ passes }">
       <b-form 
         @submit.prevent="passes(onSubmit)" 
-        method="POST" 
-        data-netlify="true"
         name="weekender-2-registration"
       >
         <ValidationProvider tag="div" name="Name" rules="required" mode="lazy" v-slot="{ valid, errors }">
@@ -48,9 +46,17 @@
             required
           ></b-form-select>
         </b-form-group>
-        <b-button type="submit" block variant="dark" class="float-right">Register</b-button>
+        <b-button type="submit" block variant="dark" class="float-right" ref="submitBtn">Register</b-button>
       </b-form>
     </ValidationObserver>
+    <div class="loading" v-if="submitting">
+      <div class="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -66,6 +72,7 @@ export default {
   },
   data() {
     return {
+      submitting: false,
       systems: {
         dayOne: [
           "None",
@@ -99,53 +106,27 @@ export default {
         .join("&");
     },
     onSubmit() {
-      const vm = this;
-      const axiosConfig = {
-        header: { "Content-Type": "application/x-www-form-urlencoded" }
-      };
+      this.submitting = true;
+      this.$refs.submitBtn.blur();
       axios
-        .post(
-          `/thanks?name=${vm.registrationData.name}`,
-          vm.encode({
-            "form-name": "weekender-2-registration",
-            ...vm.registrationData
-          }),
-          axiosConfig
-        )
-        .then(() => {
-          console.log(vm.registrationData.name);
+        .post("https://script.google.com/a/toptablegaming.com/macros/s/AKfycbxgCtXkk3kUij8bb20ortATSeeLJ2whkY9dmNEV/exec", this.encode(this.registrationData))
+        .then(response => {
+          console.log(response.data);
           this.$router.push({
             name: "thanks",
             params: {
-              name: vm.registrationData.name
+              name: this.registrationData.name
             },
             query: { 
-              name: vm.registrationData.name 
+              name: this.registrationData.name 
             }
           });
+          this.submitting = false;
         })
         .catch(error => {
           console.log(error);
-          vm.errored = true;
+          this.errored = true;
         });
-      // axios
-      //   .get("//www.mocky.io/v2/5d81420c30000069006995c6")
-      //   .then(response => {
-      //     console.log(response.data.name);
-      //     this.$router.push({
-      //       name: "thanks",
-      //       params: {
-      //         name: response.data.name
-      //       },
-      //       query: { 
-      //         name: response.data.name 
-      //       }
-      //     });
-      //   })
-      //   .catch(error => {
-      //     console.log(error);
-      //     this.errored = true;
-      //   });
     }
   }
 };
@@ -155,10 +136,82 @@ export default {
 .register-form {
   width: 100%;
   float: left;
+  position: relative;
+}
+.register-form form {
+  position: relative;
+  z-index: 1;
 }
 .register-form .btn-primary {
   background: #e53e3e;
   border-color: #e53e3e;
   color: #fff;
+}
+.loading {
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255,255,255,0.9);
+}
+.loader {
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  top: 50%;
+  left: 50%;
+  margin: -32px 0 0 -32px;
+}
+.loader div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background:#e53e3e;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.loader div:nth-child(1) {
+  left: 6px;
+  animation: loading1 0.6s infinite;
+}
+.loader div:nth-child(2) {
+  left: 6px;
+  animation: loading2 0.6s infinite;
+}
+.loader div:nth-child(3) {
+  left: 26px;
+  animation: loading2 0.6s infinite;
+}
+.loader div:nth-child(4) {
+  left: 45px;
+  animation: loading3 0.6s infinite;
+}
+@keyframes loading1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes loading3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes loading2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(19px, 0);
+  }
 }
 </style>
